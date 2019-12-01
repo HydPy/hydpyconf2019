5aa603709ff810d36ddb1334c333eb97ade77bcb
Aarshad Devani <addy@Aarshads-MacBook-Pro.local>
2019-12-01 11:30:57 +0530

    adding notification related changes

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -13,54 +13,161 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 messaging.usePublicVapidKey(publicVapidKey);
-Notification.requestPermission().then(permission => {
-  if (permission === "granted") {
-    console.log("Notification permission granted.");
-    // TODO(developer): Retrieve an Instance ID token for use with FCM.
-    // ...
-    messaging
-      .getToken()
-      .then(currentToken => {
-        if (currentToken) {
-          sendTokenToServer(currentToken);
-          updateUIForPushEnabled(currentToken);
-        } else {
-          // Show permission request.
-          console.log(
-            "No Instance ID token available. Request permission to generate one."
-          );
-          // Show permission UI.
-          updateUIForPushPermissionRequired();
-          setTokenSentToServer(false);
-        }
-      })
-      .catch(err => {
-        console.log("An error occurred while retrieving token. ", err);
-        showToken("Error retrieving Instance ID token. ", err);
-        setTokenSentToServer(false);
-      });
 
-    // Callback fired if Instance ID token is updated.
-    messaging.onTokenRefresh(() => {
+const tokenDivId = "token_div";
+const permissionDivId = "permission_div";
+
+messaging.onTokenRefresh(() => {
+  messaging
+    .getToken()
+    .then(refreshedToken => {
+      console.log("Token refreshed.");
+      setTokenSentToServer(false);
+      sendTokenToServer(refreshedToken);
+      // Display new Instance ID token and clear UI of all previous messages.
+      resetUI();
+    })
+    .catch(err => {
+      console.log("Unable to retrieve refreshed token ", err);
+      showToken("Unable to retrieve refreshed token ", err);
+    });
+});
+
+messaging.onMessage(payload => {
+  console.log("Message received. ", payload);
+  // Update the UI to include the received message.
+  appendMessage(payload);
+});
+
+function resetUI() {
+  clearMessages();
+  showToken("loading...");
+  // [START get_token]
+  // Get Instance ID token. Initially this makes a network call, once retrieved
+  // subsequent calls to getToken will return from cache.
+  messaging
+    .getToken()
+    .then(currentToken => {
+      if (currentToken) {
+        sendTokenToServer(currentToken);
+        updateUIForPushEnabled(currentToken);
+      } else {
+        // Show permission request.
+        console.log(
+          "No Instance ID token available. Request permission to generate one."
+        );
+        // Show permission UI.
+        updateUIForPushPermissionRequired();
+        setTokenSentToServer(false);
+      }
+    })
+    .catch(err => {
+      console.log("An error occurred while retrieving token. ", err);
+      showToken("Error retrieving Instance ID token. ", err);
+      setTokenSentToServer(false);
+    });
+}
+
+// [TODO] - write code here to show on UI, this is not exactly notification handled
+function showToken(currentToken) {
+  // Show token in console and UI.
+  const tokenElement = document.querySelector("#token");
+  tokenElement.textContent = currentToken;
+}
+
+function sendTokenToServer(currentToken) {
+  if (!isTokenSentToServer()) {
+    console.log("Sending token to server...");
+    // TODO(developer): Send the current token to your server.
+    setTokenSentToServer(true);
+  } else {
+    console.log(
+      "Token already sent to server so won't send it again " +
+        "unless it changes"
+    );
+  }
+}
+function isTokenSentToServer() {
+  return window.localStorage.getItem("sentToServer") === "1";
+}
+
+function setTokenSentToServer(sent) {
+  window.localStorage.setItem("sentToServer", sent ? "1" : "0");
+}
+
+function showHideDiv(divId, show) {
+  const div = document.querySelector("#" + divId);
+  if (show) {
+    div.style = "display: visible";
+  } else {
+    div.style = "display: none";
+  }
+}
+
+function requestPermission() {
+  console.log("Requesting permission...");
+  // [START request_permission]
+  Notification.requestPermission().then(permission => {
+    if (permission === "granted") {
+      console.log("Notification permission granted.");
+      resetUI();
+    } else {
+      console.log("Unable to get permission to notify.");
+    }
+  });
+}
+
+function deleteToken() {
+  // Delete Instance ID token.
+  messaging
+    .getToken()
+    .then(currentToken => {
       messaging
-        .getToken()
-        .then(refreshedToken => {
-          console.log("Token refreshed.");
-          // Indicate that the new Instance ID token has not yet been sent to the
-          // app server.
+        .deleteToken(currentToken)
+        .then(() => {
+          console.log("Token deleted.");
           setTokenSentToServer(false);
-          // Send Instance ID token to app server.
-          sendTokenToServer(refreshedToken);
-          // ...
+          // Once token is deleted update UI.
+          resetUI();
         })
         .catch(err => {
-          console.log("Unable to retrieve refreshed token ", err);
-          showToken("Unable to retrieve refreshed token ", err);
+          console.log("Unable to delete token. ", err);
         });
+    })
+    .catch(err => {
+      console.log("Error retrieving Instance ID token. ", err);
+      showToken("Error retrieving Instance ID token. ", err);
     });
-  } else {
-    console.log("Unable to get permission to notify.");
+}
+
+// [TODO] - Write code to show custom UI Notification for website
+function appendMessage(payload) {
+  const messagesElement = document.querySelector("#messages");
+  const dataHeaderELement = document.createElement("h5");
+  const dataElement = document.createElement("pre");
+  dataElement.style = "overflow-x:hidden;";
+  dataHeaderELement.textContent = "Received message:";
+  dataElement.textContent = JSON.stringify(payload, null, 2);
+  messagesElement.appendChild(dataHeaderELement);
+  messagesElement.appendChild(dataElement);
+}
+
+function clearMessages() {
+  const messagesElement = document.querySelector("#messages");
+  while (messagesElement.hasChildNodes()) {
+    messagesElement.removeChild(messagesElement.lastChild);
   }
-});
-// Get Instance ID token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
+}
+
+function updateUIForPushEnabled(currentToken) {
+  showHideDiv(tokenDivId, true);
+  showHideDiv(permissionDivId, false);
+  showToken(currentToken);
+}
+
+function updateUIForPushPermissionRequired() {
+  showHideDiv(tokenDivId, false);
+  showHideDiv(permissionDivId, true);
+}
+
+resetUI();
--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,76 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+importScripts("https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.5.0/firebase-messaging.js");
 
 if (workbox) {
-
   workbox.routing.registerRoute(
     /\.(?:js|css)$/,
     new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'static-resources',
+      cacheName: "static-resources"
     })
   );
-  
   workbox.routing.registerRoute(
     // Cache image files.
     /\.(?:png|jpg|jpeg|svg|gif)$/,
     // Use the cache if it's available.
     new workbox.strategies.CacheFirst({
       // Use a custom cache name.
-      cacheName: 'image-cache',
+      cacheName: "image-cache",
       plugins: [
         new workbox.expiration.Plugin({
           // Cache only 20 images.
           maxEntries: 20,
           // Cache for a maximum of a week.
-          maxAgeSeconds: 7 * 24 * 60 * 60,
+          maxAgeSeconds: 7 * 24 * 60 * 60
         })
-      ],
+      ]
     })
   );
-
   // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
   workbox.routing.registerRoute(
     /^https:\/\/fonts\.googleapis\.com/,
     new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'google-fonts-stylesheets',
+      cacheName: "google-fonts-stylesheets"
     })
   );
-
   // Cache the underlying font files with a cache-first strategy for 1 year.
   workbox.routing.registerRoute(
     /^https:\/\/fonts\.gstatic\.com/,
     new workbox.strategies.CacheFirst({
-      cacheName: 'google-fonts-webfonts',
+      cacheName: "google-fonts-webfonts",
       plugins: [
         new workbox.cacheableResponse.Plugin({
-          statuses: [0, 200],
+          statuses: [0, 200]
         }),
         new workbox.expiration.Plugin({
           maxAgeSeconds: 60 * 60 * 24 * 365,
-          maxEntries: 30,
-        }),
-      ],
+          maxEntries: 30
+        })
+      ]
     })
   );
-
 }
+
+firebase.initializeApp({
+  messagingSenderId: "254977934750"
+});
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Background Message Title";
+  const notificationOptions = {
+    body: "Background Message body.",
+    icon: "/firebase-logo.png"
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
