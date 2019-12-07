7840264a67d3983cbcf968354b6a224c156503f5
Ananya Maiti <ananyoevo@gmail.com>
2019-12-07 08:19:00 +0530

    Comment counter js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,51 +16,51 @@ $(document).ready(function() {
     });
 
     /* ======= Countdown ========= */
-    // set the date we're counting down to
-    var target_date = new Date("Dec 7, 2019").getTime();
+    // // set the date we're counting down to
+    // var target_date = new Date("Dec 7, 2019").getTime();
 
-    // variables for time units
-    var days, hours, minutes, seconds;
+    // // variables for time units
+    // var days, hours, minutes, seconds;
 
-    // get tag element
-    var countdown =  document.getElementById("countdown-box");
-    var days_span = document.createElement("SPAN");
-    days_span.className = 'days';
-    countdown.appendChild(days_span);
-    var hours_span = document.createElement("SPAN");
-    hours_span.className = 'hours';
-    countdown.appendChild(hours_span);
-    var minutes_span = document.createElement("SPAN");
-    minutes_span.className = 'minutes';
-    countdown.appendChild(minutes_span);
-    var secs_span = document.createElement("SPAN");
-    secs_span.className = 'secs';
-    countdown.appendChild(secs_span);
+    // // get tag element
+    // var countdown =  document.getElementById("countdown-box");
+    // var days_span = document.createElement("SPAN");
+    // days_span.className = 'days';
+    // countdown.appendChild(days_span);
+    // var hours_span = document.createElement("SPAN");
+    // hours_span.className = 'hours';
+    // countdown.appendChild(hours_span);
+    // var minutes_span = document.createElement("SPAN");
+    // minutes_span.className = 'minutes';
+    // countdown.appendChild(minutes_span);
+    // var secs_span = document.createElement("SPAN");
+    // secs_span.className = 'secs';
+    // countdown.appendChild(secs_span);
 
-    // update the tag with id "countdown" every 1 second
-    setInterval(function () {
+    // // update the tag with id "countdown" every 1 second
+    // setInterval(function () {
 
-        // find the amount of "seconds" between now and target
-        var current_date = new Date().getTime();
-        var seconds_left = (target_date - current_date) / 1000;
+    //     // find the amount of "seconds" between now and target
+    //     var current_date = new Date().getTime();
+    //     var seconds_left = (target_date - current_date) / 1000;
 
-        // do some time calculations
-        days = parseInt(seconds_left / 86400);
-        seconds_left = seconds_left % 86400;
+    //     // do some time calculations
+    //     days = parseInt(seconds_left / 86400);
+    //     seconds_left = seconds_left % 86400;
 
-        hours = parseInt(seconds_left / 3600);
-        seconds_left = seconds_left % 3600;
+    //     hours = parseInt(seconds_left / 3600);
+    //     seconds_left = seconds_left % 3600;
 
-        minutes = parseInt(seconds_left / 60);
-        seconds = parseInt(seconds_left % 60);
+    //     minutes = parseInt(seconds_left / 60);
+    //     seconds = parseInt(seconds_left % 60);
 
-        // format countdown string + set tag value.
-        days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit">Days</span>';
-        hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit">Hrs</span>';
-        minutes_span.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit">Mins</span>';
-        secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit">Secs</span>';
+    //     // format countdown string + set tag value.
+    //     days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit">Days</span>';
+    //     hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit">Hrs</span>';
+    //     minutes_span.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit">Mins</span>';
+    //     secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit">Secs</span>';
 
-    }, 1000);
+    // }, 1000);
 
     /* ======= Modal Speaker ========= */
     // https://getbootstrap.com/docs/4.3/components/modal/#events
